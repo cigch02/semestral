eb7e180110d1cd59f0a6776b51b5c466fd2738d9
Valerie2799 <valerie.tunon@utp.ac.pa>
2021-12-09 02:58:26 -0500

    Sensor de Luz

---
+++ app/src/main/java/com/example/semestral/luz_activity.java
@@ -0,0 +1,57 @@
+package com.example.semestral;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+public class luz_activity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private TextView lightLevel;
+    ImageView foquito_a;
+    ImageView foquito_e;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lightLevel = (TextView) findViewById(R.id.light_level);
+        foquito_a = (ImageView) findViewById(R.id.apagado);
+        foquito_e = (ImageView) findViewById(R.id.encendido);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sensorManager != null) {
+            sensorManager.unregisterListener(listener);
+        }
+    }
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+// The value of the first subscript in the values array is the current light intensity
+            float value = event.values[0];
+            lightLevel.setText("El nivel de Luz es " + value + " lx");
+            if (value == 0) {
+                foquito_a.setVisibility(View.VISIBLE);
+                foquito_e.setVisibility(View.INVISIBLE);
+            } else {
+                foquito_e.setVisibility(View.VISIBLE);
+                foquito_a.setVisibility(View.INVISIBLE);
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+}
