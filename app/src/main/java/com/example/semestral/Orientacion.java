42fee4b38320dbb2f07a3570e68d32a51b8fa67c
Totowo17 <95728903+Totowo17@users.noreply.github.com>
2021-12-08 22:04:14 -0500

    Activity para sensor orientacion

---
+++ app/src/main/java/com/example/semestral/Orientacion.java
@@ -0,0 +1,14 @@
+package com.example.semestral;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Orientacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orientacion);
+    }
+}
