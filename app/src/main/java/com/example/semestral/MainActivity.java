531eb0625e8401c3b73cbf19e0189c9cedde5b02
Valerie2799 <valerie.tunon@utp.ac.pa>
2021-12-08 23:32:55 -0500

    mejora de colores del main activity

--- app/src/main/java/com/example/semestral/MainActivity.java
+++ app/src/main/java/com/example/semestral/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        getSupportActionBar().hide(); //hide the title bar.
         iG = new Intent(this,gravedad.class);
         Gb =  findViewById(R.id.Gravedad);
 
